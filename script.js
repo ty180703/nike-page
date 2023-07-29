6cc07d376c50f20235dfc07a72a88c6c06a3a162
ty180703 <tyreseedwards03@gmail.com>
2023-07-30 00:34:51 +0100

    using api

---
+++ script.js
@@ -0,0 +1,35 @@
+let minValue = document.getElementById("min-value");
+let maxValue = document.getElementById("max-value");
+
+function validateRange(minPrice, maxPrice) {
+  if (minPrice > maxPrice) {
+    // Swap to Values
+    let tempValue = maxPrice;
+    maxPrice = minPrice;
+    minPrice = tempValue;
+  }
+
+  minValue.innerHTML = "$" + minPrice;
+  maxValue.innerHTML = "$" + maxPrice;
+}
+
+const inputElements = document.querySelectorAll("input");
+
+inputElements.forEach((element) => {
+  element.addEventListener("change", (e) => {
+    let minPrice = parseInt(inputElements[0].value);
+    let maxPrice = parseInt(inputElements[1].value);
+
+    validateRange(minPrice, maxPrice);
+  });
+});
+
+validateRange(inputElements[0].value, inputElements[1].value);
+
+$(document).ready(function () {
+  $.getJSON("./data.json", function (data) {
+    console.log("helo");
+  }).fail(function () {
+    console.log("check ya code");
+  });
+});
